1270f22f7f5f663e133cb416ee7b67752e1accb2
Francis Bonacic <francisbonacic@icloud.com>
2020-08-10 15:46:19 -0700

    initial push

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -11,7 +11,9 @@ We can only feature one awesome programmer at a time.
 Find comments below to help you along.
 */
 
-import React from 'react';
+import React ,{useState} from 'react';
+
+
 
 // Use this variable ONLY to initialize a slice of state!
 // There is something in the JSX right now breaking this rule.
@@ -27,6 +29,10 @@ const listOfAwesome = [
 export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers on the one hand, and the id of the featured programmer on the other.
+  const [listObject, setListObject] = useState(listOfAwesome);
+  const [idValue, setIdValue] = useState();
+
+
 
   const getNameOfFeatured = () => {
     // This is not an event handler but a helper function. See its usage below.
@@ -49,7 +55,7 @@ export default function Programmers() {
           /* Nasty bug! We should map over a slice of state, instead of 'listOfAwesome'.
           We might say: "it works, though!" But if the list of programmers is not state,
           we could never add or edit programmers in the future. The list would be a static thing. ;)" */
-          listOfAwesome.map(dev =>
+          listObject.map(dev =>
             <div key={dev.id}>
               {dev.name} <button onClick={() => { /* set the featured id passing dev.id */ }}>Feature</button>
             </div>
