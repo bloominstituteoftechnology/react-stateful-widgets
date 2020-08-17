2f3197744f7d78fe6fedf85fbb04610acbe9b8a1
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-17 12:10:09 +0200

    add tests

--- cypress/integration/Counter.spec.js
+++ cypress/integration/Counter.spec.js
@@ -1,11 +1,6 @@
-context('ActionZXYA', () => {
-  beforeEach(() => {
+describe('Counter', () => {
+  it('navigate to the site', () => {
     cy.visit('http://localhost:1234')
-  })
-
-  it('has the proper heading', () => {
-    cy.get('.widget-counter h2')
-      .should('have.text', 'Counter')
   })
   it('has an increment button', () => {
     cy.contains('Increment')
@@ -15,5 +10,28 @@ context('ActionZXYA', () => {
   })
   it('has a reset button', () => {
     cy.contains('Reset')
+  })
+  it('displays the correct initial message', () => {
+    cy.contains('Number 0 is even', { matchCase: false })
+  })
+  it('increments the count', () => {
+    cy.contains('Increment').click()
+    cy.contains('Number 0 is even', { matchCase: false }).should('not.exist')
+
+    cy.contains('Number 1 is odd', { matchCase: false })
+    cy.contains('Increment').click()
+    cy.contains('Number 2 is even', { matchCase: false })
+    cy.contains('Increment').click()
+    cy.contains('Number 3 is odd', { matchCase: false })
+  })
+  it('decrements the count', () => {
+    cy.contains('Decrement').click()
+    cy.contains('Number 2 is even', { matchCase: false })
+    cy.contains('Decrement').click()
+    cy.contains('Number 1 is odd', { matchCase: false })
+  })
+  it('resets the count', () => {
+    cy.contains('Reset').click()
+    cy.contains('Number 0 is even', { matchCase: false })
   })
 })
