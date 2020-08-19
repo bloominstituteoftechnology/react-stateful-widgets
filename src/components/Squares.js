ecdbb0aac17f3c871567dac8a16ca7d8c86b4f4f
karmaeiic <46303520+karmaeiic@users.noreply.github.com>
2020-08-19 16:59:13 -0700

    squares.js update

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -24,6 +24,9 @@ export default function Squares() {
   // 'activeSquare'. One holds the _array_ of square ids, and the other keeps track
   // of the currently active square (if any).
 
+  const [squares, setSquares] = setState(listOfSquareIds);
+  const [activeSquare, setActiveSquare] = setState();
+
   const getClassName = id => {
     // This is NOT a click handler but a helper, used inside the JSX (see below).
     // It should return a string containing the class name of 'active', if the id passed
@@ -37,6 +40,7 @@ export default function Squares() {
     // Set the id argument to become the active id in state
     // (unless it already is, in which case we should reset
     // the currently active square id back to initial state).
+    return activeSquare === id ? "active" : ""
   };
 
   return (
@@ -47,11 +51,11 @@ export default function Squares() {
           /* Nasty bug! We should map over a slice of state, instead of 'listOfSquareIds'.
           We might say: "it works, though!" But if the list of squares is not state,
           we could never add squares, change squares or remove squares in the future. Fix!" */
-          listOfSquareIds.map(id =>
+          squares.map(id =>
             <div
               id={id}
               key={id}
-              className={`square${getClassName(id)}`}
+              className={`square${isActive(id)}`}
               onClick={() => markActive(id)}
             >
             </div>
