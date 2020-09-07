7dc9bec7d289c3d6a62007dd409bbcea8aaf11cd
Joshua Whitwell <josh.whitwell@gmail.com>
2020-09-07 17:02:13 -0400

    added solution to spinner.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -37,23 +37,23 @@ STEP 4:
   Do you remember the operator we use to do "not"?
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react';
 
 export default function Spinner() {
-/* STEP 1 */
+  const [spinnerOn, setSpinnerOn] = useState(true)
 
   const toggleSpinner = () => {
-  /* STEP 4 */
+    setSpinnerOn(!spinnerOn)
   };
 
   return (
     <div className='widget-spinner container'>
       <h2>Spinner</h2>
       {
-        true && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
+        spinnerOn === true && <div id='spinner' className='spinner'>--+--</div>
       }
       <button id='toggleSpinner' onClick={toggleSpinner}>
-        Hide Spinner {/* STEP 3 */}
+        {spinnerOn === true ? 'Hide Spinner' : 'Show Spinner'}
       </button>
     </div>
   );
