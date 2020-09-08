a187e6c1be141fc7800f858b0ca527161d6052eb
LBU <lbu@LBUui-MacBook-Pro.local>
2020-09-08 16:36:33 -0400

    fixed color change

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -64,7 +64,7 @@ export default function Counter() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: {count} % 2 === 0 ? 'royalblue' : 'crimson' /* STEP 2 */
+    color: count % 2 === 0 ? 'royalblue' : 'crimson' /* STEP 2 */
   };
 
   return (
