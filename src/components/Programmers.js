af06a95c690c293aacd2bf3ab8ce41ad487423cb
Roy-Weiss <67227056+Roy-Weiss@users.noreply.github.com>
2020-09-16 22:03:15 -0400

    Programmers

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 /*
 PROGRAMMERS Instructions
 
@@ -11,7 +12,7 @@ We can only feature one awesome programmer at a time.
 Find comments below to help you along.
 */
 
-import React from 'react';
+import React, {useState} from 'react';
 
 // Use this variable ONLY to initialize a slice of state!
 // There is something in the JSX right now breaking this rule...
@@ -27,6 +28,8 @@ export const listOfAwesome = [
 export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
+  const [programmers, setProgrammers] = useState(listOfAwesome)
+  const [id, setId] = useState(null)
 
   const getNameOfFeatured = () => {
     // Leave this for last!
