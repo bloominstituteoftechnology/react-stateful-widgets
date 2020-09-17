96f9d5f0322f0c64fdc96179f8189f54e9d03c76
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-09-16 17:07:36 -0700

    Moods

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,8 +46,7 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
-import theCount, { useState } from "react";
+import React, {useState} from 'react'; /* STEP 0 */
 export default function Counter() {
   /* STEP 1 */
   const [count, setCount] = useState(0);
