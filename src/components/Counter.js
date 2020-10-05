683d35300de0eca72386d5279a98ef90d90500d7
kayla felts <kayla.thurman1127@gmail.com>
2020-10-05 17:40:26 -0400

    almost done wth spinner

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -68,7 +68,7 @@ const [count, setCount] = useState(0)
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: (count%2 === 0) ? 'royalblue' : 'crimson', /* STEP 2 */
+    color: count%2 === 0 ? 'royalblue' : 'crimson', /* STEP 2 */
   };
 
   return (
