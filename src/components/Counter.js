c031cabfb58bd4b406c681c4ed2d959dc3336e7f
Andreas Balourdas <dreyuy@outlook.com>
2020-10-06 06:08:59 -0700

    final

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,36 +46,53 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
 
 export default function Counter() {
   /* STEP 1 */
-
+  const [counter ,setCounter] = useState(0)
+  const [isEven, setEven] = useState('even')
+  
   const increment = () => {
+    setCounter(counter + 1)
     /* STEP 4 */
   };
   const decrement = () => {
+    setCounter(counter - 1)
     /* STEP 5 */
   };
   const reset = () => {
+    setCounter(counter == '0')
     /* STEP 6 */
   };
+  const oddEven = () => {
+    (counter % 2 == 1) ? setEven('Even') : setEven('Odd');
+  }
 
-  const style = {
+  
+  
+
+
+  const styleBlue = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color:(counter%2 == 1) ? 'royalblue' : 'crimson',
+     /* STEP 2 */
   };
+  
 
   return (
     <div className='widget-counter container'>
       <h2>Counter</h2>
-      <div id='count' style={style}>
-        Number 0 is even {/* STEP 3 */}
+      <div id='countEven' style={styleBlue}>
+        Number {counter} is {isEven} {/* STEP 3 */}
       </div>
+      {/* <div id='countOdd' style={styleRed}>
+        Number {counter} is Odd {/* STEP 3 */}
+      
       <div>
-        <button id='increment' onClick={increment}>Increment</button>
-        <button id='decrement' onClick={decrement}>Decrement</button>
+        <button id='increment' onClick={() => { increment(); oddEven();}}>Increment</button>
+        <button id='decrement' onClick={() => { decrement(); oddEven();}}>Decrement</button>
         <button id='resetCount' onClick={reset}>Reset</button>
       </div>
     </div>
--- src/components/Moods.js
+++ src/components/Moods.js
@@ -28,7 +28,7 @@ STEPS 4, 5, 6:
   Inside these click handlers set the correct mood, using 'setMood' and the variables below the imports.
 */
 
-import React from 'react'; /* STEP 0 */
+import React , {useState} from 'react'; /* STEP 0 */
 
 const initialMood = 'Not sure how I feel';
 const happyMood = 'Quite happy!';
@@ -36,27 +36,30 @@ const sadMood = 'Rather sad';
 
 export default function Moods() {
   /* STEP 1 */
-
+   const [myMood, setMyMood] = useState(initialMood)
   const makeHappy = () => {
     /* STEP 4 */
+    setMyMood(happyMood)
   };
   const makeSad = () => {
     /* STEP 5 */
+    setMyMood(sadMood)
   };
   const reset = () => {
     /* STEP 6 */
+    setMyMood(initialMood)
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'crimson', /* STEP 2 */
+    color: (myMood == happyMood) ?'crimson' : 'royalblue' /* STEP 2 */
   };
 
   return (
     <div className='widget-moods container'>
       <h2>Moods</h2>
-      <div id='mood' style={style}>Not sure how I feel</div> {/* STEP 3 */}
+  <div id='mood' style={style}>{myMood}</div> {/* STEP 3 */}
       <div>
         <button id='makeHappy' onClick={makeHappy}>Make Happy</button>
         <button id='makeSad' onClick={makeSad}>Make Sad</button>
