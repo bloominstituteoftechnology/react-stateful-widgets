a36952032eb35ceeb4106b13d92b05777170264f
Danielle Koduru <69174710+DanielleKoduru@users.noreply.github.com>
2020-10-14 16:58:22 -0600

    changes

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -18,7 +18,7 @@ STEP 1:
   The 'mood' slice of state should be initialized to one of the three variables right below our imports.
   Those variables exist so we don't need to write those long strings anywhere inside the component.
 
-STEP 2:
+STEP 2: 
   Make the color of the text be royalblue if the state of the mood is happy, crimson otherwise.
 
 STEP 3:
@@ -28,7 +28,7 @@ STEPS 4, 5, 6:
   Inside these click handlers set the correct mood, using 'setMood' and the variables below the imports.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0 */
 
 const initialMood = 'Not sure how I feel';
 const happyMood = 'Quite happy!';
@@ -36,27 +36,31 @@ const sadMood = 'Rather sad';
 
 export default function Moods() {
   /* STEP 1 */
+  const [mood, setMood] = useState(initialMood);
 
   const makeHappy = () => {
     /* STEP 4 */
+    setMood(mood == happyMood)
   };
   const makeSad = () => {
     /* STEP 5 */
+    setMood(mood == sadMood)
   };
   const reset = () => {
     /* STEP 6 */
+    setMood(mood == initialMood)
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'crimson', /* STEP 2 */
+    color: `${mood === happyMood ? 'royalblue' : 'crimson'}`, /* STEP 2 */
   };
 
   return (
     <div className='widget-moods container'>
       <h2>Moods</h2>
-      <div id='mood' style={style}>Not sure how I feel</div> {/* STEP 3 */}
+      <div id={mood} style={style}>Not sure how I feel</div> {/* STEP 3 */}
       <div>
         <button id='makeHappy' onClick={makeHappy}>Make Happy</button>
         <button id='makeSad' onClick={makeSad}>Make Sad</button>
