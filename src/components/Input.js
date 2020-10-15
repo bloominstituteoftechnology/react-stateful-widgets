40919297e7323109a2b7bfdc30adbd658af9f852
xandre9-git <jmjames673@gmail.com>
2020-10-14 20:17:36 -0400

    completed the mood.js portion of module

--- src/components/Input.js
+++ src/components/Input.js
@@ -38,6 +38,7 @@ import React from 'react'; /* STEP 0 */
 
 export default function Input() {
   /* STEP 1 */
+  const [inputValue, setInputValue] = ""
 
   const changeInput = evt => {
     // When the input changes, its whole value can be found inside the event object.
