8788ebb6c8402eed3bce13bfc9a6c19e1f607f3d
MaurooRen <maurooren@gmail.com>
2020-12-08 10:57:08 -0500

    rendering celebrate message

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -68,9 +68,9 @@ export default function Programmers() {
           // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
           // Pseudo-code: if the currently featured id is truthy render text 1, otherwise render text 2.
           // Replace the hard-coded false with the correct variable.
-          false
-            ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳`
-            : 'Pick an awesome programmer'
+          currently != null
+          ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳`
+          : 'Pick an awesome programmer'
         }
       </div>
     </div>
