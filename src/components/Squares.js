f90a2da56e488e23ca729c66b440f8a82d2817ce
Rhiannon Stanford <rhiannonstanford@gmail.com>
2020-12-16 15:00:56 -0800

    Complete squares section

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -14,7 +14,7 @@ Only one square (or none) can be active at any given point.
 Find comments below to help you along.
 */
 
-import React from 'react';
+import React, { useState } from 'react';
 
 // Use this variable ONLY to initialize a slice of state!
 const listOfSquareIds = ['sqA', 'sqB', 'sqC', 'sqD'];
@@ -24,21 +24,55 @@ export default function Squares() {
   // 'activeSquare'. One holds the _array_ of square ids, and the other keeps track
   // of the currently active square. On page load there's no active square,
   // so the value of 'activeSquare' should be null.
+  const [squares, setSquares] = useState(listOfSquareIds);
+  const [activeSquare, setActiveSquare] = useState(null);
 
   const getClassName = id => {
     // This is NOT a click handler but a helper, used inside the JSX (see below).
     // It should return a string containing the class name of 'active', if the id passed
     // as the argument matches the active square in state, empty string otherwise.
     // Right-click and "inspect element" on the square to see its effect.
-    return ''
+    // let squareInQuestion = document.querySelector(id);
+
+  //   if (id === activeSquare) {
+  //     return "active"
+  //   } else {
+  //     return "";
+  //   };
+  // };
+
+  return `${id === activeSquare ? "active" : ""}`;
+
   };
+
+  // function isActiveSquare(id) {
+  //   let squareInQuestion = document.querySelector(id);
+
+  //   if (squareInQuestion === undefined) { // if squareInQuestion doesn't exist
+  //     return false;
+  //   } else {
+  //     return squareInQuestion.classList.contains(active);
+  //   }
+    
+  // }; // I thought this helper function might have been better to determine if the square is selected active aka. has class name 'active'.  Takes id, returns T/F
 
   const markActive = id => {
     // This is a helper used inside an _inlined_ click handler (see below).
     // Set the id argument to become the active id in state
     // (unless it already is, in which case we should reset
     // the currently active square id back to initial state).
-  };
+  //   if (getClassName(id) === "" ) {
+  //     setActiveSquare(id);
+  //     console.log("State: ", activeSquare);
+  //   } else if (getClassName(id) === "active") {
+  //     setActiveSquare(null);
+  //   };
+  // };
+
+  id === activeSquare ? setActiveSquare(null) : setActiveSquare(id);
+  console.log("State: ", activeSquare);
+
+}
 
   return (
     <div className='widget-squares container'>
@@ -48,11 +82,11 @@ export default function Squares() {
           // Nasty bug! We should map over a slice of state, instead of 'listOfSquareIds'.
           // We might say: "it works, though!" But if the list of squares is not state,
           // we could never add squares, change squares or remove squares in the future. Fix!
-          listOfSquareIds.map(id =>
+          squares.map(id =>
             <div
               id={id}
               key={id}
-              className={`square${getClassName(id)}`}
+              className={`square ${getClassName(id)}`}
               onClick={() => markActive(id)}
             >
             </div>
