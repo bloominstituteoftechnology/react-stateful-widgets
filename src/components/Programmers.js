5301fc16aafeeeb271f90d1ffcb5f97f839dded3
Tarah <tarah.agbokhana@gmail.com>
2021-02-14 07:32:40 -0500

    Squares cleanup

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -11,7 +11,7 @@ We can only feature one awesome programmer at a time.
 Find comments below to help you along.
 */
 
-import React from 'react';
+import React, {useState} from 'react';
 
 // Use this variable ONLY to initialize a slice of state!
 // There is something in the JSX right now breaking this rule...
@@ -27,7 +27,8 @@ export const listOfAwesome = [
 export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
-
+  var [programmer, setProgrammer] = useState(null);
+  
   const getNameOfFeatured = () => {
     // Leave this for last!
     // This is NOT an event handler but a helper function. See its usage inside the JSX.
