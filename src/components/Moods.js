7b77c51a956ef0c824a0de9daa83510c1bc93e19
ErikBurdett <erikaburdet@gmail.com>
2021-02-15 21:37:01 -0600

    4/6 working correctly but I'm lost on the squares and programmers and just kinda done for today. The structure of things lately has been really off and it's taking it's toll on me lmao

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -10,53 +10,57 @@ the things that change in this widget? Give it some thought before continuing re
 Some might say 3 different states: one per mood (happy, sad, and uncertain).
 But a single slice of state is more than enough to keep track of all these moods!
 
-STEP 0:
+STEP 0: x
   Study the component below, and import the state hook.
 
-STEP 1:
+STEP 1:x
   Create a 'mood', 'setMood' pair using the state hook.
   The 'mood' slice of state should be initialized to one of the three variables right below our imports.
   Those variables exist so we don't need to write those long strings anywhere inside the component.
 
-STEP 2:
+STEP 2:x
   Make the color of the text be royalblue if the state of the mood is happy, crimson otherwise.
 
-STEP 3:
+STEP 3:x
   Remove the hard-coded mood and interpolate the 'mood' slice of state instead, using curly brackets.
 
-STEPS 4, 5, 6:
+STEPS 4, 5, 6:x
   Inside these click handlers set the correct mood, using 'setMood' and the variables below the imports.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
 
 const initialMood = 'Not sure how I feel';
 const happyMood = 'Quite happy!';
 const sadMood = 'Rather sad';
 
 export default function Moods() {
+  const [mood, setMood] = useState(initialMood);
   /* STEP 1 */
 
   const makeHappy = () => {
+    setMood(happyMood);
     /* STEP 4 */
   };
   const makeSad = () => {
+    setMood(sadMood);
     /* STEP 5 */
   };
   const reset = () => {
+    setMood(initialMood);
     /* STEP 6 */
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'crimson', /* STEP 2 */
+    color: mood===happyMood ? 'royalblue':'crimson'
   };
 
   return (
     <div className='widget-moods container'>
       <h2>Moods</h2>
-      <div id='mood' style={style}>Not sure how I feel</div> {/* STEP 3 */}
+      <div id='mood' style={style}>{mood}</div> {/* STEP 3 */}
       <div>
         <button id='makeHappy' onClick={makeHappy}>Make Happy</button>
         <button id='makeSad' onClick={makeSad}>Make Sad</button>
