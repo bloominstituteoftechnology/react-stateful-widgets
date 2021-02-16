b08c992eb337c3761acfe9b74e2ca414efa2d797
L-Steinmacher <dasai.sacul@gmail.com>
2021-02-15 19:52:56 -0800

    finished mvp, starting stretch

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -39,7 +39,15 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-    return 
+    let name = ''
+    developers.forEach( dev => {
+      
+      if(activeId === dev.id){
+       console.log('its working', dev.name)
+       name =  dev.name;
+      }
+    })
+    return name;
   };
 
   const style = {
