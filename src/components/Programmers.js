59054431c8f7a88c1d351dc37a73e2da354ea429
Michael Cavellier <tyrannicalnoobz@gmail.com>
2021-02-16 01:06:56 -0500

    done

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -1,17 +1,15 @@
 /*
 PROGRAMMERS Instructions
-
 Watch this short video:
 https://tk-assets.lambdaschool.com/1ea6e6a2-2ef1-45de-bcab-b99a8f775870_programmers.gif
-
 This component keeps track of a list of pioneers in the field of programming on the one hand,
 and the id of the currently featured programmer on the other. That's two slices of state!
 We can only feature one awesome programmer at a time.
-
 Find comments below to help you along.
 */
 
 import React from 'react';
+import React, {useState} from 'react';
 
 // Use this variable ONLY to initialize a slice of state!
 // There is something in the JSX right now breaking this rule...
@@ -23,10 +21,11 @@ export const listOfAwesome = [
   { id: '5', name: 'Frances Allen' },
   { id: '6', name: 'Carol Shaw' },
 ];
-
 export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
+  const [programmers, na] = useState(listOfAwesome)
+  const [currentProgrammer, changeProgrammer] = useState()
 
   const getNameOfFeatured = () => {
     // Leave this for last!
@@ -34,12 +33,14 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
+    return currentProgrammer.name
   };
 
   const style = {
     fontSize: '1.5em',
     marginTop: '0.5em',
     color: 'royalblue', // 🤔 color turns to gold, when celebrating
+    color: currentProgrammer ? 'gold' : 'royalBlue'
   };
 
   return (
@@ -51,8 +52,10 @@ export default function Programmers() {
           We might think: "it works, though!" But if the list of programmers is not state,
           we could never add or edit programmers in the future. The list would be a static thing." */
           listOfAwesome.map(dev =>
+          programmers.map(dev =>
             <div className='programmer' key={dev.id}>
               {dev.name} <button onClick={() => { /* in here set the featured id to be dev.id */ }}>Feature</button>
+              {dev.name} <button onClick={() => changeProgrammer(dev)}>Feature</button>
             </div>
           )
         }
@@ -63,6 +66,7 @@ export default function Programmers() {
           // Pseudo-code: if the currently featured id is truthy render text 1, otherwise render text 2.
           // Replace the hard-coded false with the correct variable.
           false
+          currentProgrammer
             ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳`
             : 'Pick an awesome programmer'
         }
