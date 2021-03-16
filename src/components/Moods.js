67a9300eb4a9882cde9a409cffd0cce08fbf82ee
Shamal Williams <shamal@Shamals-MacBook-Pro.local>
2021-03-16 01:52:39 -0400

    MoodJSNotRespondingVeryConfused

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -28,7 +28,7 @@ STEPS 4, 5, 6:
   Inside these click handlers set the correct mood, using 'setMood' and the variables below the imports.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
 
 const initialMood = 'Not sure how I feel';
 const happyMood = 'Quite happy!';
@@ -36,21 +36,29 @@ const sadMood = 'Rather sad';
 
 export default function Moods() {
   /* STEP 1 */
+  const[mood, setMood]=useState(initialMood)
+
 
   const makeHappy = () => {
+    setMood(happyMood)
+
     /* STEP 4 */
   };
   const makeSad = () => {
+    setMood(sadMood)
+
     /* STEP 5 */
   };
   const reset = () => {
+    setMood(initialMood)
+
     /* STEP 6 */
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'crimson', /* STEP 2 */
+    color: mood == happyMood ? 'red' :'indigo', /* STEP 2 */
   };
 
   return (
