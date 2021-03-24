8169df222db609a81bcd35da72ff4dbb9562f6e8
Nicole Pizzano <nicolepizzano@Nicoles-iMac.local>
2021-03-24 16:19:12 -0400

    save

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,11 +46,16 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+// import React from 'react'; /* STEP 0 */
 
-export default function Counter() {
-  /* STEP 1 */
+import React, { useState } from 'react';
 
+
+export default function Counter(props) {
+ return<div>
+   <h3>Hello from Counter</h3>
+ </div>
+ 
   const increment = () => {
     /* STEP 4 */
   };
