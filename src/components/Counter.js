c1acd5f750dbc8035e77ad970ce0c537a4aef4bd
unknown <twessar42@gmail.com>
2021-03-25 12:17:13 -0700

    adjsuted styling and syntax

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -70,13 +70,13 @@ export default function Counter() {
   return (
     <div className='widget-counter container'>
       <h2>Counter</h2>
-      <div id='count' style={style}>
-        Number { count } is { ( count % 2 == 0 ) ? 'even' : 'odd' } {/* STEP 3 */}
+      <div id='count' style={ style }>
+        Number { count } is { count % 2 === 0 ? 'even' : 'odd' } {/* STEP 3 */}
       </div>
       <div>
-        <button id='increment' onClick={increment}>Increment</button>
-        <button id='decrement' onClick={decrement}>Decrement</button>
-        <button id='resetCount' onClick={reset}>Reset</button>
+        <button id='increment' onClick={ increment }>Increment</button>
+        <button id='decrement' onClick={ decrement }>Decrement</button>
+        <button id='resetCount' onClick={ reset }>Reset</button>
       </div>
     </div>
   );
