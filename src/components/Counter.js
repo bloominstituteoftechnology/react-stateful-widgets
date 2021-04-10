736181e5b0ef8aa95f42dad62c49065649ae9ce1
MichaelHabermas <michaelghabermas@gmail.com>
2021-04-09 17:51:46 -0700

    counter done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,43 +46,32 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Counter() {
-	/* STEP 1   Using the state hook, create a 'count', 'setCount' pair.
-  The 'count' state should be initialized to the number zero.*/
+	const [count, setCount] = useState(0);
 
 	const increment = () => {
-		/* STEP 4  This click handler needs to use 'setCount' to schedule the 'count' to become the current 'count' plus one.
-  These state changes are not synchronous: the updated count arrives on the next run of the Counter component.
-  Do NOT simply do count++. The plus plus is forbidden! We never mutate a slice of state in place. Even if you could
-  reassign a const, React would not be aware anything changed. Always use the state updater, passing in a new value.*/
+		setCount(count + 1);
 	};
 	const decrement = () => {
-		/* STEP 5  This click handler needs to use 'setCount' to set the 'count' to be the current 'count' minus one.
-  Do NOT do count--. That amounts to trying to mutate 'count' in place. This is the road to perdition.*/
+		setCount(count - 1);
 	};
 	const reset = () => {
-		/* STEP 6 This click handler needs to use 'setCount' to set the 'count' to be zero again.*/
+		setCount(0);
 	};
 
 	const style = {
 		fontSize: '1.5em',
 		marginBottom: '0.3em',
-		color:
-			'royalblue' /* STEP 2  The 'style' object has the 'color' property hard-coded to "royalblue".
-  What the value of 'color' should be instead is a ternary expression that goes like this:
-  If count is even, then "royalblue", else "crimson".*/
+		color: `${count % 2 === 0 ? 'royalblue' : 'crimson'}`
 	};
 
 	return (
 		<div className="widget-counter container">
 			<h2>Counter</h2>
 			<div id="count" style={style}>
-				Number 0 is even{' '}
-				{/* STEP 3 We need to replace some hard-coded info in the JSX with expressions, interpolated inside curly brackets.
-  Start by replacing the character "0" with {count}. The 'count' slice of state is the source of truth here.
-  Then, replace the word "even" with a ternary: {if count is even number, then string "even", else string "odd"}.*/}
+				Number {count} is {count % 2 === 0 ? 'even' : 'odd'}
 			</div>
 			<div>
 				<button id="increment" onClick={increment}>
