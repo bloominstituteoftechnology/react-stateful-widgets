15c7b9a7ade965fbbaaf6c2b6c644524194d5098
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-04-12 16:55:43 -0600

    update to input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -44,7 +44,7 @@ export default function Input() {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
     const { value } = evt.target;
-
+      setInputValue( value )
     /* STEP 4 */
   };
   const reset = () => {
@@ -61,11 +61,9 @@ export default function Input() {
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={style.toUpperCase}>
-          <div>{inputValue}</div>
-        </div> 
+      <div id='output' style={style}>{inputValue.toUpperCase()}</div> 
       <div>
-        <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
+        <input id='input' type='text' onChange={changeInput} value={inputValue} /> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
       </div>
     </div>
