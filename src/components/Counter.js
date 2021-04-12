a22eb911ef86db22ca804a61eaa49d91f24e7e3b
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-04-12 17:26:34 -0600

    update to counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -75,7 +75,7 @@ export default function Counter() {
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number {count} is  { count ? 0 == 10 : 'even' } {count ? 1 == 10 : 'odd'}. 
+        Number {count} is  {(count % 2 == 0) ? 'even' : 'odd'}. 
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
