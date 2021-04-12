ed8d9adada2fd93d1fd3436f0a361e2f1f223044
Josh Levan <joshlevan@Joshs-iMac-2.local>
2021-04-12 15:25:15 -0400

    input complete

--- src/components/Input.js
+++ src/components/Input.js
@@ -44,11 +44,11 @@ export default function Input() {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
     const { value } = evt.target;
-
+    setInputValue(value);
     /* STEP 4 */
   };
   const reset = () => {
-    /* STEP 5 */
+    setInputValue('');
   };
 
   const style = {
@@ -61,9 +61,9 @@ export default function Input() {
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={style}></div> {/* STEP 3 */}
+      <div id='output' style={style}>{ inputValue }</div> {/* STEP 3 */}
       <div>
-        <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
+        <input id='input' type='text' onChange={changeInput} value={inputValue} /> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
       </div>
     </div>
