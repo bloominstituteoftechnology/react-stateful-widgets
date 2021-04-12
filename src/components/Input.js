2cb6cdabeb0dd5ff62251dea6cf483800fae4632
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-04-12 17:08:42 -0400

    completed first widget

--- src/components/Input.js
+++ src/components/Input.js
@@ -34,10 +34,11 @@ STEP 6:
   We need to add an extra prop to the <input /> element like so: value={inputValue}
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
 
 export default function Input() {
-  /* STEP 1 */
+  
+  const [inputValue, setInputValue] = useState("")
 
   const changeInput = evt => {
     // When the input changes, its whole value can be found inside the event object.
@@ -56,10 +57,12 @@ export default function Input() {
     color: 'royalblue', /* STEP 2 */
   };
 
+  {inputValue.length > 10 ? style.color = 'crimson' : style.color = 'royalblue'}
+
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={style}></div> {/* STEP 3 */}
+      <div id='output' style={style}></div> {inputValue}
       <div>
         <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
