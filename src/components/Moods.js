bdef1964e9d06e4fc03d8ec05ee0751b2bf6813e
cynthiaannberg <cynthia.ann.berg@me.com>
2021-04-12 21:33:59 -0600

    react

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -28,11 +28,13 @@ STEPS 4, 5, 6:
   Inside these click handlers set the correct mood, using 'setMood' and the variables below the imports.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
+// const [mood, setMood] = useState(0);
 
 const initialMood = 'Not sure how I feel';
 const happyMood = 'Quite happy!';
 const sadMood = 'Rather sad';
+
 
 export default function Moods() {
   /* STEP 1 */
--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -11,7 +11,7 @@ We can only feature one awesome programmer at a time.
 Find comments below to help you along.
 */
 
-import React from 'react';
+import React, {useState} from 'react';;
 
 // Use this variable ONLY to initialize a slice of state!
 // There is something in the JSX right now breaking this rule...
@@ -52,7 +52,8 @@ export default function Programmers() {
           we could never add or edit programmers in the future. The list would be a static thing." */
           listOfAwesome.map(dev =>
             <div className='programmer' key={dev.id}>
-              {dev.name} <button onClick={() => { /* in here set the featured id to be dev.id */ }}>Feature</button>
+              {dev.name} <button onClick={() => { /* in here set the featured id to be dev.id */ 
+              setFeaturedId(dev.id)}}>Feature</button>
             </div>
           )
         }
@@ -62,9 +63,9 @@ export default function Programmers() {
           // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
           // Pseudo-code: if the currently featured id is truthy render text 1, otherwise render text 2.
           // Replace the hard-coded false with the correct variable.
-          false
-            ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳`
-            : 'Pick an awesome programmer'
+          false(featuredID === '') ? `Pick an awesome programmer`: `🎉 Let's celebrate
+           ${getNameOfFeatured()}! 🥳`
+            
         }
       </div>
     </div>
