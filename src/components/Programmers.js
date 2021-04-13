53d9a6e2bf1767de91ba4d257b66f7c4ba793963
Hugo-Orozco <aceasin@outlook.com>
2021-04-12 21:00:42 -0600

    Finished Project

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -28,12 +28,17 @@ export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
 
+  const [list] = React.useState(listOfAwesome);
+
+  const [programmer, setProgrammer] = React.useState(null);
+
   const getNameOfFeatured = () => {
     // Leave this for last!
     // This is NOT an event handler but a helper function. See its usage inside the JSX.
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
+    return list.find(object => object.id === programmer).name;
   };
 
   const style = {
@@ -41,6 +46,13 @@ export default function Programmers() {
     marginTop: '0.5em',
     color: 'royalblue', // 🤔 color turns to gold, when celebrating
   };
+
+  if (programmer) {
+    style.color = 'gold';
+  }
+  else {
+    style.color = 'royalblue';
+  }
 
   return (
     <div className='widget-programmers container'>
@@ -50,9 +62,9 @@ export default function Programmers() {
           /* Nasty bug! We should map over a slice of state, instead of 'listOfAwesome'.
           We might think: "it works, though!" But if the list of programmers is not state,
           we could never add or edit programmers in the future. The list would be a static thing." */
-          listOfAwesome.map(dev =>
+          list.map(dev =>
             <div className='programmer' key={dev.id}>
-              {dev.name} <button onClick={() => { /* in here set the featured id to be dev.id */ }}>Feature</button>
+              {dev.name} <button onClick={() => setProgrammer(dev.id)}>Feature</button>
             </div>
           )
         }
@@ -62,7 +74,7 @@ export default function Programmers() {
           // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
           // Pseudo-code: if the currently featured id is truthy render text 1, otherwise render text 2.
           // Replace the hard-coded false with the correct variable.
-          false
+          programmer
             ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳`
             : 'Pick an awesome programmer'
         }
--- src/components/Squares.js
+++ src/components/Squares.js
@@ -25,12 +25,21 @@ export default function Squares() {
   // of the currently active square. On page load there's no active square,
   // so the value of 'activeSquare' should be null.
 
+  const [squares] = React.useState(listOfSquareIds);
+
+  const [activeSquare, setActiveSquare] = React.useState(null);
+
   const getClassName = id => {
     // This is NOT a click handler but a helper, used inside the JSX (see below).
     // It should return a string containing the class name of 'active', if the id passed
     // as the argument matches the active square in state, empty string otherwise.
     // Right-click and "inspect element" on the square to see its effect.
-    return ''
+    if (id === activeSquare) {
+      return 'active';
+    }
+    else {
+      return '';
+    }
   };
 
   const markActive = id => {
@@ -38,6 +47,12 @@ export default function Squares() {
     // Set the id argument to become the active id in state
     // (unless it already is, in which case we should reset
     // the currently active square id back to initial state).
+    if (activeSquare === id) {
+      setActiveSquare(null);
+    }
+    else {
+      setActiveSquare(id);
+    }
   };
 
   return (
@@ -48,11 +63,11 @@ export default function Squares() {
           // Nasty bug! We should map over a slice of state, instead of 'listOfSquareIds'.
           // We might say: "it works, though!" But if the list of squares is not state,
           // we could never add squares, change squares or remove squares in the future. Fix!
-          listOfSquareIds.map(id =>
+          squares.map(id =>
             <div
               id={id}
               key={id}
-              className={`square${getClassName(id)}`}
+              className={`square ${getClassName(id)}`}
               onClick={() => markActive(id)}
             >
             </div>
