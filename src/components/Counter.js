5df8a5d1f24f1538271ea947740d422aa43e3ef7
MichaelHabermas <michaelghabermas@gmail.com>
2021-04-12 17:45:22 -0700

    MVP

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -64,7 +64,8 @@ export default function Counter() {
 	const style = {
 		fontSize: '1.5em',
 		marginBottom: '0.3em',
-		color: `${count % 2 === 0 ? 'royalblue' : 'crimson'}`
+		// color: `${count % 2 === 0 ? 'royalblue' : 'crimson'}`
+		color: count % 2 === 0 ? 'royalblue' : 'crimson'
 	};
 
 	return (
