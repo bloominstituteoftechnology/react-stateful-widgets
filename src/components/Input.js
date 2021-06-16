e1e33d0d8558265a7f6fd76fc3cc5e582b3b7ae1
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-06-16 14:25:20 -0500

    finally got the input.js to work. i was missing the interpolation of inputValue

--- src/components/Input.js
+++ src/components/Input.js
@@ -42,26 +42,26 @@ export default function Input() {
   const changeInput = evt => {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
-    const { value } = evt.target;
-
-    /* STEP 4 */
+    const { value } = evt.target
+    setInputValue(value)
+   
   };
   const reset = () => {
-    /* STEP 5 */
+    setInputValue("")
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: (inputValue >= 10) ? 'crimson':'royalblue'
+    color: inputValue.length > 10 ? 'crimson':'royalblue'
   };
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={style}>{inputValue.toUpperCase()}</div> {/* STEP 3 */}
+      <div id='output' style={style}>{inputValue.toUpperCase()}</div>
       <div>
-        <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
+        <input id='input' type='text' onChange={changeInput}/> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
       </div>
     </div>
