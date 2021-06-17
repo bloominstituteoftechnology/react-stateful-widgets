290d2736ee5c6a7a3aac0956d833c69475e0436a
Daniel <dek.kneale@gmail.com>
2021-06-16 19:58:50 -0700

    a functional spinner Widget

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -37,12 +37,14 @@ STEP 4:
   Do you remember the operator we use to do "not"?
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
 
 export default function Spinner() {
+  const [ spinnerOn,setSpinnerOn ] = useState(true) 
 /* STEP 1 */
 
   const toggleSpinner = () => {
+    setSpinnerOn(!spinnerOn)
   /* STEP 4 */
   };
 
@@ -50,10 +52,10 @@ export default function Spinner() {
     <div className='widget-spinner container'>
       <h2>Spinner</h2>
       {
-        true && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
+        spinnerOn && <div id='spinner' className='spinner'>---+---</div> /* STEP 2 */
       }
       <button id='toggleSpinner' onClick={toggleSpinner}>
-        Hide Spinner {/* STEP 3 */}
+        { spinnerOn ? "Hide" : "Show"} Spinner{/* STEP 3 */}
       </button>
     </div>
   );
