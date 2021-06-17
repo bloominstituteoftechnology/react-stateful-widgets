a594b952b51906e077e00d1760cb3135e5508ec3
Gabe Chilson <g.chilson34@gmail.com>
2021-06-16 20:52:08 -0600

    finished 4 out of 6 tasks

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -68,7 +68,7 @@ export default function Counter() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    // count % 2 === 0 ? color: 'royalblue' : 'crimson'/* STEP 2 */
+    color: count % 2 === 0 ? 'royalblue' : 'crimson'/* STEP 2 */
   };
 
   return (
