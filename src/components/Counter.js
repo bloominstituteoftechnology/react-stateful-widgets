64b8218dd7571218abef7ad4b4083c21c0618d29
christina-yun <poncheeyun@yahoo.com>
2021-08-09 13:06:47 -0700

    Finished task 2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -51,6 +51,7 @@ import React, { useState } from 'react'; /* STEP 0 */
 export default function Counter() {
   /* STEP 1 */
   const [count, setCount] = useState(0);
+
   const increment = () => {
     /* STEP 4 */
     setCount(count + 1);
@@ -67,7 +68,7 @@ export default function Counter() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: count % 2 === 0 ?'royalblue' : 'crimson', /* STEP 2 */
+    color: count % 2 === 0 ? 'royalblue' : 'crimson', /* STEP 2 */
   };
 
   return (
