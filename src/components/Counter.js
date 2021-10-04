a6ec9eb414c1adc24d76f6ed7ddb7841de99c284
Dave Madden <davelearnsstuff@gmail.com>
2021-10-04 11:35:42 -0700

    counter works. m oving to moods

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -72,7 +72,7 @@ export default function Counter(props) {
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number {count} is even {/* STEP 3 */}
+        Number {count} is {(count % 2) ? "ODD" : "EVEN"} {/* STEP 3 */}
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
