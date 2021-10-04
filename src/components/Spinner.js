f54b492ab86ca95fff5d300c6e51e2309737e970
Hannah Brog <bugsbrog@Hannahs-MBP.home>
2021-10-04 15:19:56 -0600

    Done with Counter.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -37,13 +37,15 @@ STEP 4:
   Do you remember the operator we use to do "not"?
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Spinner() {
 /* STEP 1 */
+const [spinnerOn, setSpinnerOn] = useState(true)
 
   const toggleSpinner = () => {
   /* STEP 4 */
+  setSpinnerOn(!spinnerOn)
   };
 
   return (
