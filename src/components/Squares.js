ac35b94c51d2b1036ca67f244cdad147e9a06445
Ryan Howard <ryanghoward@protonmail.com>
2021-10-04 14:10:15 -0700

    Squares in progress

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -14,7 +14,7 @@ Only one square (or none) can be active at any given point.
 Find comments below to help you along.
 */
 
-import React from 'react';
+import React, { useState } from 'react';
 
 // Use this variable ONLY to initialize a slice of state!
 const listOfSquareIds = ['sqA', 'sqB', 'sqC', 'sqD'];
