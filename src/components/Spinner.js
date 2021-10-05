7e8d6352f85f998e0f8b6c4760d0f4be87151eef
Hughes <hughespham2002the2nd@yahoo.com>
2021-10-04 19:17:50 -0500

    1/2 done

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -41,7 +41,7 @@ import React from 'react'; /* STEP 0 */
 
 export default function Spinner() {
 /* STEP 1 */
-
+  const [spinnerOn, setSpinnerOn] = useState(true);
   const toggleSpinner = () => {
   /* STEP 4 */
   };
