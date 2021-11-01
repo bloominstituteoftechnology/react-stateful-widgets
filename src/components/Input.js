7f648d56b2e03992943209d34a893586b73f606f
Ryan Howard <ryanghoward@protonmail.com>
2021-11-01 13:43:12 -0700

    Step 6 Input.js done

--- src/components/Input.js
+++ src/components/Input.js
@@ -63,7 +63,7 @@ export default function Input() {
       <h2>Input</h2>
       <div id='output' style={style}>{inputValue.toUpperCase()}</div> {/* STEP 3 */}
       <div>
-        <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
+        <input id='input' type='text' onChange={changeInput} value={inputValue} /> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
       </div>
     </div>
