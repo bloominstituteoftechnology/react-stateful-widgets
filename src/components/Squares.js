3cc5e8e51151f0e6aef48642351c8c3ce95407e9
SheebaHashmi <sheebahashmi0@gmail.com>
2021-11-01 17:54:27 -0400

    Finish the homework

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -41,8 +41,7 @@ export default function Squares() {
     // Set the id argument to become the active id in state
     // (unless it already is, in which case we should reset
     // the currently active square id back to initial state).
-    // const activeSquareReturn = id === activeSquare?  null: id;
-    // setActiveSquare(activeSquareReturn)
+   
      activeSquare === id? setActiveSquare(null):setActiveSquare(id);
   };
 
