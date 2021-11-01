d5c53d7e85ab680ad09553f7023e0953f412dbf8
Ryan Howard <ryanghoward@protonmail.com>
2021-11-01 12:46:03 -0700

    Step 2 Counter.js done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -63,7 +63,7 @@ export default function Counter() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: (count % 2 === 0) ? 'royalblue' : 'crimson', /* STEP 2 */
   };
 
   return (
