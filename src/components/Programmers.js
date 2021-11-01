aceaaaa966cd11f6ed59680256f6ccd590cdb14c
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-11-01 16:38:51 -0600

    started mvp

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -25,15 +25,20 @@ export const listOfAwesome = [
 ];
 
 export default function Programmers() {
+  const [programmerName, setProgrammerName] = useState(listOfAwesome);
+  const [programmerId, setProgrammerId] = useState(null);
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
 
   const getNameOfFeatured = () => {
+
+
     // Leave this for last!
     // This is NOT an event handler but a helper function. See its usage inside the JSX.
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
+    return list[id- i].name
   };
 
   const style = {
@@ -52,7 +57,7 @@ export default function Programmers() {
           we could never add or edit programmers in the future. The list would be a static thing." */
           listOfAwesome.map(dev =>
             <div className='programmer' key={dev.id}>
-              {dev.name} <button onClick={() => { /* in here set the featured id to be dev.id */ }}>Feature</button>
+              {dev.name} <button onClick={() => { setProgrammerId(dev.name)}}>Feature</button>
             </div>
           )
         }
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -37,14 +37,16 @@ STEP 4:
   Do you remember the operator we use to do "not"?
 */
 
-import React from 'react'; /* STEP 0 */
+import React , { useState } from 'react'; /* STEP 0 */
 
 export default function Spinner() {
 /* STEP 1 */
+  const [spinnerOn, setSpinnerOn] = useState(true);
 
   const toggleSpinner = () => {
   /* STEP 4 */
-  };
+  setSpinnerOn(!spinnerOn)
+};
 
   return (
     <div className='widget-spinner container'>
@@ -53,7 +55,7 @@ export default function Spinner() {
         true && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
       }
       <button id='toggleSpinner' onClick={toggleSpinner}>
-        Hide Spinner {/* STEP 3 */}
+        Hide Spinner {/* STEP 3 */spinnerOn === true ? "hide spinner" : "show spinner"}
       </button>
     </div>
   );
