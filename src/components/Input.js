8ed6406359caf2d48c1f023ce9e44aaecbb361b1
Richard Dowd <richarddowd@Richards-MacBook-Pro.local>
2021-12-06 15:28:01 -0800

    moods complete

--- src/components/Input.js
+++ src/components/Input.js
@@ -56,7 +56,7 @@ export default function Input() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: inputValue > 10 ? 'crimson' : 'royalblue', /* STEP 2 */
+    color: inputValue.length > 10 ? 'crimson' : 'royalblue', /* STEP 2 */
   };
 
   return (
