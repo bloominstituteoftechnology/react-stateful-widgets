d643eecd0a9893c75eca715532e89d9b05c760fd
Shamir Gibbons <nonshalant@shamirs-air.myfiosgateway.com>
2021-12-06 20:42:49 -0500

    counter

--- src/components/Input.js
+++ src/components/Input.js
@@ -34,15 +34,17 @@ STEP 6:
   We need to add an extra prop to the <input /> element like so: value={inputValue}
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
 
 export default function Input() {
   /* STEP 1 */
 
+  const [inputValue, setInputValue] = useState('');
+
   const changeInput = evt => {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
-    const { value } = evt.target;
+    const {} = evt.target;
 
     /* STEP 4 */
   };
