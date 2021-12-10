6ea9d59d53fc1ca82e16b363cac8f9e1a9cf46af
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2021-12-10 05:36:44 -0800

    add

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -55,13 +55,13 @@ export default function Counter() {
   /* STEP 1 */
 
   const increment = () => {
-    /* STEP 4 */
+    /* STEP 4 */ setCount(count + 1);
   };
   const decrement = () => {
-    /* STEP 5 */
+    /* STEP 5 */ setCount(count - 1);
   };
   const reset = () => {
-    /* STEP 6 */
+    /* STEP 6 */ setCount(0);
   };
 
   const style = {
@@ -74,7 +74,7 @@ export default function Counter() {
     <div className="widget-counter container">
       <h2>Counter</h2>
       <div id="count" style={style}>
-        Number 0 is even {/* STEP 3 */}
+        Number {count} is {count % 2 === 0 ? "even" : "odd"} {/* STEP 3 */}
       </div>
       <div>
         <button id="increment" onClick={increment}>
