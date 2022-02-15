ae16f1a2600c3f65af634e8d59db05de0635eaec
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-02-14 22:54:30 -0800

    completed counter to step 5

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -15,24 +15,24 @@ A naive developer might say 3 different slices:
 But a single slice of state is all that's needed here: the count!
 The other things can simply be _derived_ from the count itself.
 
-STEP 0:
+////STEP 0:
   Start by studying the component below, and importing the state hook.
 
-STEP 1:
+////STEP 1:
   Using the state hook, create a 'count', 'setCount' pair.
   The 'count' state should be initialized to the number zero.
 
-STEP 2:
+////STEP 2:
   The 'style' object has the 'color' property hard-coded to "royalblue".
   What the value of 'color' should be instead is a ternary expression that goes like this:
   If count is even, then "royalblue", else "crimson".
 
-STEP 3:
+////STEP 3:
   We need to replace some hard-coded info in the JSX with expressions, interpolated inside curly brackets.
   Start by replacing the character "0" with {count}. The 'count' slice of state is the source of truth here.
   Then, replace the word "even" with a ternary: {if count is even number, then string "even", else string "odd"}.
 
-STEP 4:
+////STEP 4:
   This click handler needs to use 'setCount' to schedule the 'count' to become the current 'count' plus one.
   These state changes are not synchronous: the updated count arrives on the next run of the Counter component.
   Do NOT simply do count++. The plus plus is forbidden! We never mutate a slice of state in place. Even if you could
@@ -46,13 +46,21 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
+
+
+const even = ( num ) => num % 2 === 0 ? true : false;
+
 
 export default function Counter() {
+
   /* STEP 1 */
+  const [count, setCount] = useState(0);
+
 
   const increment = () => {
     /* STEP 4 */
+    onClick = setCount(count + 1)
   };
   const decrement = () => {
     /* STEP 5 */
@@ -64,14 +72,15 @@ export default function Counter() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: {count = even ? "royalblue" : "crimson"} /* STEP 2 */
   };
 
   return (
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number 0 is even {/* STEP 3 */}
+        {/* STEP 3 */}
+        Number 0 is {count = even ? "even" ? "odd"}  
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
