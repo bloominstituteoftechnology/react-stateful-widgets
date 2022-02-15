ef0a3c48de252212e09d03ebbe2e21c8d05ed845
dawson <dawsonreschke@icloud.com>
2022-02-14 23:40:18 -0800

    fixed a bug causing the INPUT.js input text to be red with only 10 character when it should turn red when over 10.

--- src/components/Input.js
+++ src/components/Input.js
@@ -53,7 +53,7 @@ export default function Input() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: inputValue.length < 10 ? 'royalblue':'crimson', /* STEP 2 */
+    color: inputValue.length <= 10 ? 'royalblue':'crimson', /* STEP 2 */
   };
 
   return (
