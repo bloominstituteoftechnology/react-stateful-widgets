794bbbb1b1eaefcd489cdfc187dd04d669417903
Marie Knowlton <mtknowlton67@gmail.com>
2022-03-01 08:36:54 -0800

    Completed Programmers.js Component

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -24,9 +24,8 @@ export const listOfAwesome = [
 export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
-  const [programmersValue, setProgrammersValue] = useState(listOfAwesome);
-  const[featuredPerson, setFeaturedPerson] = useState(null);
-
+  const [programmers] = useState(listOfAwesome);
+  const [featured, setFeatured] = useState(null);
 
   const getNameOfFeatured = () => {
     // Leave this for last!
@@ -34,13 +33,13 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-    return programmersValue[id - 1].name;
+    return programmers.find(prog => prog.id === featured).name;
   };
 
   const style = {
     fontSize: '1.5em',
     marginTop: '0.5em',
-    color: featuredPerson != null ? 'gold' : 'royalblue' // 🤔 color turns to gold, when celebrating
+    color: featured ? 'gold' : 'royalblue', // 🤔 color turns to gold, when celebrating
   };
 
   return (
@@ -51,9 +50,9 @@ export default function Programmers() {
           /* Nasty bug! We should map over a slice of state, instead of 'listOfAwesome'.
           We might think: "it works, though!" But if the list of programmers is not state,
           we could never add or edit programmers in the future. The list would be a static thing." */
-          Programmers.map(dev =>
+          programmers.map(dev =>
             <div className='programmer' key={dev.id}>
-              {dev.name} <button onClick={() => setFeaturedPerson(dev.id)}>Feature</button>
+              {dev.name} <button onClick={() => { setFeatured(dev.id); /* in here set the featured id to be dev.id */ }}>Feature</button>
             </div>
           )
         }
@@ -63,7 +62,7 @@ export default function Programmers() {
           // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
           // Pseudo-code: if the currently featured id is truthy render text 1, otherwise render text 2.
           // Replace the hard-coded false with the correct variable.
-          featuredPerson
+          featured
             ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳`
             : 'Pick an awesome programmer'
         }
