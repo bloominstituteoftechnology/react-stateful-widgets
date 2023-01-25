d906cf80549d966153f5dd186d105f7450021b28
Aaron Belmore <aaronbelmore@Aarons-MBP.lan>
2023-01-25 10:49:06 -0500

    completed Counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -53,13 +53,14 @@ export default function Counter() {
   const [count, setCount] = useState(0)
 
   const increment = () => {
-    /* STEP 4 */
+    setCount(count + 1);
   };
   const decrement = () => {
     /* STEP 5 */
+    setCount(count + 1);
   };
   const reset = () => {
-    /* STEP 6 */
+    setCount(count = 0);
   };
 
   const style = {
