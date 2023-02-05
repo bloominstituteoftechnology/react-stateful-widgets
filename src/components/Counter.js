df1dc3912b33bb2cddc874570bfdef961f7fe23b
DJ1 <jimmynoble604@gmail.com>
2023-02-04 20:20:51 -0500

    step three is updated

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -72,7 +72,7 @@ const [count, setCount] = (0);
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number {count} is even {/* STEP 3 */}
+        Number {count} is {count % 2 === 0 ? 'even' : 'odd'} {/* STEP 3 */}
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
