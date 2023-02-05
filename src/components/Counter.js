60da540d65b4c6efc6c87617293a8021c298eb30
DJ1 <jimmynoble604@gmail.com>
2023-02-04 20:00:36 -0500

    color count step three complete

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -64,7 +64,7 @@ const [count, setCount] = (0);
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: count % 2 === 0 ? 'royalblue' : 'crimson'  /* STEP 2 */
   };
 
   return (
